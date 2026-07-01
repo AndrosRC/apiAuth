0a2efa78e6412aa517f7e13d4d08dc8c39c67b42
AndrosRC <octaviodejesusruizcontreras0@gmail.com>
2026-07-01 00:11:42 -0600

    API primera version

--- api/index.py
+++ api/index.py
@@ -1,6 +1,24 @@
-from flask import Blueprint, request, jsonify
+import os
+from flask import Flask, Blueprint, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+# Importamos las funciones desde el archivo Functions.py local
 from .Functions import generate_secret, update_status, get_all_records, verify_totp, delete_record
 
+load_dotenv()
+
+# Inicialización
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Definición del Blueprint
 api_bp = Blueprint('api', __name__)
 
 @api_bp.route('/api/request', methods=['POST'])
@@ -24,3 +42,6 @@ def handle_verify():
     data = request.json
     result = verify_totp(data.get('id'), data.get('pin'))
     return jsonify(result), (200 if result['success'] else 400)
+
+# Registramos el blueprint en la app
+app.register_blueprint(api_bp)
